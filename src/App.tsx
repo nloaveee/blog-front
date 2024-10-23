26b190a9b323f7b391cef81e8a61850552c0bb35
hyeonheee <nloavee@gmail.com>
2024-10-23 10:51:24 +0900

    컴포넌트 생성

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import BoardListItem from './components/BordListItem';
+
+function App() {
+    return (
+        <>
+            <BoardListItem />
+        </>
+    );
+}
+
+export default App;
