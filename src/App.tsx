2f7826428a2fdda1fd172c25686dea08106fada1
hyeonheee <nloavee@gmail.com>
2024-10-25 09:54:59 +0900

    컴포넌트 생성 (좋아요 리스트 아이템)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
 import BoardItem from 'components/BoardItem';
-import { commentListMock, latestBoardListMock, top3BoardListMock } from 'mocks';
+import { commentListMock, favoriteListMock, latestBoardListMock, top3BoardListMock } from 'mocks';
 import Top3Item from 'components/Top3Item';
 import CommnentItem from 'components/CommentItem';
+import FavoriteItem from 'components/FavoriteItem';
 
 function App() {
     return (
         <>
-            <div style={{padding: '0 20px', display: 'flex', flexDirection: 'column', gap: '30px' }}>
-                {commentListMock.map(commentListItem => <CommnentItem commentListItem={commentListItem}/>)}
+            <div style={{display: 'flex',  columnGap: '30px' , rowGap: '20px'}}>
+                {favoriteListMock.map(favoriteListItem => <FavoriteItem favoriteListItem={favoriteListItem}/>)}
             </div>
         </>
     );
--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,9 +1,11 @@
 import top3BoardListMock from './top-3-board-list.mock';
 import latestBoardListMock from './latest-board-list.mock';
 import commentListMock from './comment-list.mock';
+import favoriteListMock from './favorite-list.mock';
 
 export {
     top3BoardListMock,
     latestBoardListMock,
-    commentListMock
+    commentListMock,
+    favoriteListMock
 }
