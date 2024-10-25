4842971cc5f27ce0fe6a7a438260b267e56ef79c
hyeonheee <nloavee@gmail.com>
2024-10-25 11:28:32 +0900

    footer layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import React, {useState} from 'react';
 import './App.css';
-import BoardItem from 'components/BoardItem';
-import { commentListMock, favoriteListMock, latestBoardListMock, top3BoardListMock } from 'mocks';
-import Top3Item from 'components/Top3Item';
-import CommnentItem from 'components/CommentItem';
-import FavoriteItem from 'components/FavoriteItem';
-import InputBox from 'components/InputBox';
+import Footer from 'layouts/Footer';
+
 
 function App() {
 
-    const [value,setValue] = useState<string>('');
-
     return (
         <>
-            <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력' value={value} error={true} setValue={setValue}/>
+            < Footer/>
         </>
     );
 }
