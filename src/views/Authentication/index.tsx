486f7937cde9736fb42790c996fafda04d6e5e09
hyeonheee <nloavee@gmail.com>
2024-10-28 11:45:15 +0900

    auth 페이지 로그인 카드

--- src/views/Authentication/index.tsx
+++ src/views/Authentication/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, KeyboardEvent, useRef } from 'react'
 import './style.css'
+import InputBox from 'components/InputBox';
 
 //            component: 인증 화면 컴포넌트                //
 export default function Authentication() {
@@ -10,9 +11,82 @@ export default function Authentication() {
     //            component: sign in card 컴포넌트                //
     const SignInCard = () => {
 
+        //            state: 이메일 요소 참조 상태              /
+        const emailRef = useRef<HTMLInputElement | null> (null);
+        //            state: 패스워드 요소 참조 상태              /
+        const passwordRef = useRef<HTMLInputElement | null> (null);
+
+
+        //            state: 이메일 상태              //
+        const [email, setEmail] = useState<string>('');
+        //            state: 패스워드 상태              //
+        const [password, setPassword] = useState<string>('');
+        //            state: 패스워드 타입 상태              //
+        const [passwordType, setPasswordType] = useState<'text' | 'password'>('password');
+        //            state: 패스워드 버튼 아이콘 상태              //
+        const [passwordButtonIcon, setPasswordButtonIcon] = useState<'eye-light-off-icon' | 'eye-light-on-icon'>('eye-light-off-icon');
+        //            state: error 상태              //
+        const [error, setError] = useState<boolean>(false);
+
+
+        //            event handler : 로그인 버튼 클릭 이벤트 처리            /
+        const onSignInButtonClickHandler = () => {}
+
+
+        //            event handler : 패스워드 버튼 클릭 이벤트 처리            //
+        const onPasswordButtonClickHandler = () => {
+            if (passwordType === 'text') {
+                setPassword('password');
+                setPasswordButtonIcon('eye-light-off-icon');
+            }
+            else {
+                setPassword('text');
+                setPasswordButtonIcon('eye-light-on-icon');
+            }
+        }
+
+        //            event handler : 이메일 인풋 키 다운 이벤트 처리            //
+        const onEmailKeyDownHandler = (event : KeyboardEvent<HTMLInputElement>) => {
+            if (event.key !== 'Enter') return;
+            if (!passwordRef.current) return;
+            passwordRef.current.focus();
+
+        }
+        
+        //            event handler : 패스워드 키 다운 이벤트 처리            //
+        const onPasswordKeyDownHandler = (event : KeyboardEvent<HTMLInputElement>) => {
+            if (event.key !== 'Enter') return;
+            onSignInButtonClickHandler();
+
+        }
+
+
+
+
         //            render:  sign in card  화면 렌더링                //
         return (
-            <div className='auth-card'></div>
+            <div className='auth-card'>
+                <div className='auth-card-box'>
+                    <div className='auth-card-top'>
+                        <div className='auth-card-title-box'>
+                            <div className='auth-card-titke'>{'로그인'}</div>
+                        </div>
+                        <InputBox ref={emailRef} label='이메일 주소' type='text' placeholder='이메일 주소를 입력해주세요' error={error} value={email} setValue={setEmail} onKeyDown={onEmailKeyDownHandler}/>
+                        <InputBox ref={passwordRef} label='패스워드' type={passwordType} placeholder='비밀번호를 입력해주세요' error={error} value={password} setValue={setPassword} icon={passwordButtonIcon} onButtonClick={onPasswordButtonClickHandler} onKeyDown={onPasswordKeyDownHandler}/>
+                    </div>
+                    <div className='auth-catd-bottom'>
+                        <div className='auth-sign-in-error-box'>
+                            <div className='auth-sign-in-error-message'>
+                                {'이메일 주소 또는 비밀번호를 잘못 입력했습니다. \n입력하신 내용을 다시 확인해주세요.'}
+                            </div>
+                        </div>
+                        <div className='black-large-full-button' onClick={onSignInButtonClickHandler}>{'로그인'}</div>
+                        <div className='auth-description-box'>
+                            <div className='auth-descrpition'>{'신규 사용자이신가요?'}<span className='auth-description-link'>{'회원가입'}</span></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     };
 
