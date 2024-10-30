53592cd3f3f571e69f625f49762b32783a80639e
hyeonheee <nloavee@gmail.com>
2024-10-30 09:09:08 +0900

    auth 페이지 회원가입 API 연동

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { SignInRequestDto, SignUpRequestDto } from './request/auth';
 import { response } from 'express';
-import { SignInResponseDto } from './response/auth';
+import { SignInResponseDto, SignUpResponseDto } from './response/auth';
 import { ResponseDto } from './response';
 
 const DOMAIN = 'http://localhost:8080';
@@ -21,11 +21,22 @@ export const signInRequest = async (requestBody: SignInRequestDto) => {
         if (!error.response.data) return null;
         const responseBody: ResponseDto = error.response.data;
         return responseBody;
-    })
+    });
 
     return result;
 }
 
 export const signUpRequest = async (requestBody: SignUpRequestDto) => {
+    const result = await axios.post(SIGN_UP_URL(), requestBody)
+    .then (response => {
+        const responseBody: SignUpResponseDto =  response.data;
+        return requestBody;
+    })
+    .catch ( error => {
+        if (!error.response.data) return null;
+        const responseBody: ResponseDto = error.response.data;
+        return responseBody;
+    });
 
+    return result;
 }
